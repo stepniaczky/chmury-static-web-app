a72f391dc168f15e59552ae12bd01ed463b9eb2a
stepniaczky <m.stepniak@yahoo.com>
2023-01-16 10:46:42 +0100

    Changing path of system env

--- api/core/settings.py
+++ api/core/settings.py
@@ -4,25 +4,18 @@ from pathlib import Path
 
 PROJECT_NAME = 'Turbo Barber'
 
-# EXPECTED SCOPES
-AUTHENTICATED = 'authenticated'
-ADMIN = 'admin'
-
-BACKEND_CORS_ORIGINS = ['http://localhost:7071',
-                        'https://turbo-barber-apiv1.azurewebsites.net']
-
-ENV_PATH = Path(__file__).parent.parent / '.env'
+ENV_PATH = Path(__file__).parent.parent.parent / '.env'
 
 # REMOTE ENV
-# CONFIG_ENV = {
-#                 'CONN_STRING': os.environ['CONN_STRING'],
-#               'DB': os.environ['DB']
-#               }
+CONFIG_ENV = {
+    'CONN_STRING': os.environ['CONN_STRING'],
+    'DB': os.environ['DB']
+}
 
 # LOCAL ENV
-CONFIG_ENV = {
-                'CONN_STRING': dotenv_values(ENV_PATH)['CONN_STRING'],
-              'DB': dotenv_values(ENV_PATH)['DB']
-              }
+# CONFIG_ENV = {
+#     'CONN_STRING': dotenv_values(ENV_PATH)['CONN_STRING'],
+#     'DB': dotenv_values(ENV_PATH)['DB']
+# }
 
 CONN_STRING = CONFIG_ENV['CONN_STRING']
